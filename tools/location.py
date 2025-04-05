f7fc0d6048084ae0f962ede5de8858256cfee357
depuntism <depuntism@gmail.com>
2025-04-05 23:37:23 +0200

    Add show weather location

--- tools/location.py
+++ tools/location.py
@@ -9,15 +9,18 @@ LAT = os.environ.get("LATITUDE")
 LON = os.environ.get("LONGITUDE")
 CITY = os.environ.get("CITY")
 IPDATA_KEY = os.environ.get("IPDATA_API_KEY")
-
+is_location_enabled = os.environ.get("ENABLE_WEATHER_LOCATION")
 
 def get_location():
-    try:
-        ipdata.api_key = IPDATA_KEY
-        data = ipdata.lookup()
-        if data["status"] == 200:
-            return data["latitude"], data["longitude"], data["city"]
-        else:
+    if is_location_enabled:
+        try:
+            ipdata.api_key = IPDATA_KEY
+            data = ipdata.lookup()
+            if data["status"] == 200:
+                return data["latitude"], data["longitude"], data["city"]
+            else:
+                return LAT, LON, CITY
+        except Exception:
             return LAT, LON, CITY
-    except Exception:
-        return LAT, LON, CITY
+    
+    return LAT, LON, CITY
